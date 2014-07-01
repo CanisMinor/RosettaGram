906bcb70a18ef7b4b41bead0beb1e7e50e056ade
CanisMinor <cjaas@massenet.home>
2014-07-01 18:48:21 +0100

    general update
    removed necessity to count input file lines, added function to find
    maximum emission count, added functionality to remove uncommon words
    with _RARE_, etc

--- emissionParam.cpp
+++ emissionParam.cpp
@@ -1,59 +1,38 @@
 //function for Part I of HW 1
+//function for Part I of HW 1
 //in Natural Language Processing course
 //Cecilia Aas June 2014
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <string>
 #include <vector>
+#include <map>
+#include <utility>
 
 using namespace std;
 
-int CountLines(string s)
+
+map<string, int> wordCounts(const string sInFile)
 {
-  int nLines=0;
-  string line;
-  ifstream count_file;
-  count_file.open(s.c_str());
-
-  while (std::getline(count_file, line)) ++nLines;
-
-  count_file.close();
-
-  cout << nLines << endl;
-
-  return nLines;
-}
-
-
-double emissionParameters()
-{
-   const string sInFile = "gene.counts";
-
-   const string sZero = "0";
-   const string sGene = "I-GENE";
-   const string sWord = "WORDTAG";
-   const string sTriGram = "3-GRAM";
-   const string sBiGram = "2-GRAM";
-   const string sUniGram = "1-GRAM";
-
-   vector<string> words;
-   vector<int> countWords;
-   vector<string> labelWords;
-
-   ofstream inCounts;
-   inCount.open(sInFile.c_str());
-   const int nLines = CountLines(sInFile);
+   const string sWordtag = "WORDTAG";
+   map <string, int>  wordCounts;  
    int nCount = 0;
-   string inString;
-   for(int i=0; i<nLines; i++)
+   string inString, inLabel, inWord;
+
+   ifstream inCount;
+   inCount.open(sInFile.c_str());
+   while(inCount.good())
    {
       inCount >> nCount >> inString;
-      if(inString == sWord)
+      if(inCount.eof()) break;
+      if(inString == sWordtag)
       {
-         inCount >> labelWords[i] >> words[i];
-         countWords[i] = nCount;
-      } 
+         inCount >> inLabel >> inWord;
+	 wordCounts[inWord] += nCount;  
+      }
       else
       {
          break;
@@ -61,6 +40,171 @@ double emissionParameters()
    }
    inCount.close();
 
+   return wordCounts;
+}
+
+
+map<vector<string>, double> emissionParameters(const string sInFile)
+{
+   const string sWordtag = "WORDTAG";
+
+   map<string, int>  wCounts = wordCounts(sInFile);  
+   
+   
+   map<vector<string>, double> emissionParams;
+   string inLabel, inWord, inString;
+   int nCount = 0;
+   ifstream inCount;
+   inCount.open(sInFile.c_str());
+   vector<string> inPuts(2,"");
+   while(inCount.good())
+   {
+      inCount >> nCount >> inString;
+      if(inCount.eof()) break;
+      if(inString == sWordtag)
+      {
+         inCount >> inLabel >> inWord;
+         inPuts[0] = inLabel;
+	 inPuts[1] = inWord;
+	 emissionParams[inPuts] = ((double) nCount) / ((double) wCounts[inWord]);
+      }
+      else
+      {
+         break;
+      }
+   }
+   inCount.close();
+
+   return emissionParams;
+}
+
+
+map<string,string> findMaxEmission(map<vector<string>, double> e)
+{
+   map<string, string> maxEmit;
+
+   const string sGene = "I-GENE";
+   const string sWord = "O";
+   vector<string> ePairGene(2,"");
+   vector<string> ePairWord(2,"");
+   typedef map<vector<string>, double>::const_iterator pIter;
+   for(pIter p = e.begin(); p!=e.end(); ++p)
+   {
+      vector<string> pLabelWord = p->first;
+      string testWord = pLabelWord[1];
+      string testLabel = pLabelWord[0];
+
+      ePairGene[0] = sGene;
+      ePairWord[0] = sWord;
+      ePairGene[1] = testWord;
+      ePairWord[1] = testWord;
+
+      if(maxEmit.count(testWord)==0)
+      {
+         if(e[ePairGene] > e[ePairWord])
+         {
+             string temp = ePairGene[0];
+	     ePairGene[0] = ePairGene[1];
+	     ePairGene[1] = temp;
+             maxEmit.insert(pair<string,string>(p->first[1],sGene));
+         }
+         else
+         {
+             string temp = ePairWord[0];
+	     ePairWord[0] = ePairWord[1];
+	     ePairGene[1] = temp;
+             maxEmit.insert(pair<string,string>(p->first[1],sWord));
+         }
+      } 
+   }
+
+   return maxEmit; 
+
+}
+
+int replaceRare()
+{
+   const string inFile = "gene.train";
+   const string outFile = "geneRare.train";
+
+   map <string, int>  wCounts = wordCounts(inFile);  
+   ofstream replaceRareOut;
+   ifstream trainIn;
+   replaceRareOut.open(outFile.c_str());
+   trainIn.open(inFile.c_str());
+
+   string inWord;
+   string inLabel;
+   while(trainIn.good())
+   {
+       trainIn >> inWord >> inLabel;
+       if(trainIn.eof() ) break;
+       if(wCounts[inWord] < 5)
+       {
+          replaceRareOut << "_RARE_" << " " << inLabel << endl;
+       }
+       else
+       {
+	  replaceRareOut << inWord << " " << inLabel << endl;
+       }
+   }
+   trainIn.close();
+   replaceRareOut.close();
+}
+
+
+int main()
+{
+   const string inFile = "geneRare.counts";
+   map<vector<string>, double> e = emissionParameters(inFile);
+
+   ofstream outCounts;
+   outCounts.open("gene_count.pl.out");
+   typedef map<vector<string>, double>::const_iterator pIter;
+   for(pIter p = e.begin(); p!=e.end(); ++p)
+   {
+       vector<string> inP = p->first;
+       outCounts << inP[0] << "\t" << inP[1] << "\t" << p->second << endl;
+   }
+   outCounts.close();
+
+
+   map<string, string> maxEmit = findMaxEmission(e);
+   ofstream maxCounts;
+   ifstream testFile;
+   testFile.open("gene.dev");
+   maxCounts.open("gene_dev.pl.out");
+   string inWord, inJunk;
+   const string sRare = "_RARE_";
+   const string sDot = ".";
+   while(testFile.good())
+   {
+       testFile >> inWord; // >> inJunk;
+       if(testFile.eof()) break;
+       if(maxEmit.count(inWord) > 0)
+       {
+          maxCounts << inWord << " " << maxEmit[inWord] << endl;
+       }
+       else
+       {
+	  maxCounts << inWord << " " << maxEmit[sRare] << endl;
+       }
+
+       if(inWord == sDot)
+       {
+	   maxCounts << endl;
+       } 
+   }
+   maxCounts.close();
+   testFile.close();
+   return 0;
+}
+
+
+/*
+
+double evalFrequencies()
+{
    inCount.open(sInFile.c_str())
    string inGram;
    vector<int> countUnigram;
@@ -81,7 +225,7 @@ double emissionParameters()
       if(inString == sWord)
       {
          continue;
-      } 
+      }
       else
       {
          inCount >> inGram;
@@ -89,8 +233,8 @@ double emissionParameters()
          if(inGram == sUniGram)
          {
             inCount >> someText;
-            sUnigram.push_back(someText); 
-            countUnigram.push_back(nCount); 
+            sUnigram.push_back(someText);
+            countUnigram.push_back(nCount);
          }
          else if(inGram == sDiGram)
          {
@@ -109,7 +253,7 @@ double emissionParameters()
             inCount >> someText;
             sTrigram3.push_back(someText);
             countTrigram.push_back(nCount);
-         } 
+         }
          else
          {
              cout << "error in reading data" << endl;
@@ -121,3 +265,5 @@ double emissionParameters()
 
    inCount.close();
 }
+
+*/
